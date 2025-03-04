e8270d8b078637013d4b4b6523da7cc6e56fcc8b
Chaden <csullivan@ascensionacademy.org>
2025-03-04 10:30:40 -0600

    first commit

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+const speed = 300.0
+
+
+
+func _process(delta: float):
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+
+	if direction.length() > 0:
+		direction = direction.normalized()
+
+		position += direction * speed * delta
